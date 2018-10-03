4607141c048d7356f133450c65aa64bad05d10db
friend1314 <2537502150@qq.com>
2018-10-03 17:57:57 +0800

    Site updated: 2018-10-03 17:57:53

---
+++ js/src/love.js
@@ -0,0 +1,77 @@
+#
+ rwl - iqxin {
+     position: fixed;
+     transform: translate(-90px, 0);
+     width: 85px;
+     height: 25px;
+     font - size: 12px;
+     font - weight: 500;
+     font - family: Verdana,
+     Arial,
+     '宋体';
+     color: #fff;
+     background: #333;z-index:2147483647;margin: 0;opacity:0.05;transition:0.3s;overflow:hidden;user-select:none;text-align:center;white-space:nowrap;line-height:25px;padding:0 16px;border:1px solid # ccc;
+     border - width: 1px 1px 1px 0;
+     border - bottom - right - radius: 5px;
+     box - sizing: content - box;
+ }#
+ rwl - iqxin input {
+     margin: 0;
+     padding: 0;
+     vertical - align: middle; - webkit - appearance: checkbox; - moz - appearance: checkbox;
+     position: static;
+     clip: auto;
+     opacity: 1;
+     cursor: pointer;
+ }#
+ rwl - iqxin.rwl - active - iqxin {
+     left: 0px;
+     transform: translate(0, 0);
+     opacity: 0.9;
+     height: 32px;
+     line - height: 32px
+ }#
+ rwl - iqxin label {
+     margin: 0;
+     padding: 0;
+     font - weight: 500;
+ }#
+ rwl - iqxin button {
+     margin: 0;
+     padding: 0 2px;
+     border: none;
+     border - radius: 2px;
+     cursor: pointer;
+ }#
+ rwl - setMenu {
+     text - align: left;
+     font - size: 14px;
+     z - index: 999999;
+     border: 1px solid cornflowerblue;
+ }#
+ rwl - setMenu p {
+     margin: 5px auto;
+ }::-moz - selection {
+     color: #000; background: none repeat scroll 100% 0% transparent;text-shadow: 0px 0px 2px # 0048FF;
+ }
+ a::selection {
+     color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 2px 2px 2px # 0048FF;
+ }::selection {
+     color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 2px 0px 2px # 0048FF;
+ }
+ input::selection {
+     color: #3356C6; background: none repeat scroll 0% 0% transparent;} a::selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 2px 0px 2px #0048FF;} ::-webkit-selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 0px 0px 2px #0048FF;} a::selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 2px 2px 2px #0048FF;} ::-ms-selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 0px 0px 2px #0048FF;} a::selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 2px 2px 2px #0048FF;} ::-o-selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 0px 0px 2px #0048FF;} a::selection { color: # 000;
+     background: none repeat scroll 0 % 0 % transparent;
+     text - shadow: 2px 2px 2px #0048FF;}
